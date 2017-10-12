f204a57a6a37fabe98e6cc0c36e5904d525f80d1
budny999 <karolbudnyy@gmail.com>
2017-10-13 00:09:30 +0200

    Zadania NIE PODGLADAĆ :D

--- entityisfun/entityisfun/Program.cs
+++ entityisfun/entityisfun/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Data.Entity.Design.EntityFrameworkVersions;
 
 namespace entityisfun
 {
